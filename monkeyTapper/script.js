0c14bde04ba3b7c2579ee8a464991b79b19386b1
Sumo-nabatame <bliao6@my.bcit.ca>
2024-11-24 15:54:30 -0800

    Update script.js
    takes a list of words and inserts it into a hidden div

--- monkeyTapper/script.js
+++ monkeyTapper/script.js
@@ -1,1 +1,21 @@
-let words = document.querySelector('placeholder')
+async function stealText() {
+    let url = 'https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_alpha.txt';
+    stolen = await fetch(url);
+    text = await stolen.text();
+    return text.split();
+    // console.log(text);
+}
+
+async function addDiv() {
+    const newdiv = document.createElement('div');
+    newdiv.setAttribute('id', 'words');
+    let words = await stealText();
+    words.forEach(word => {
+        const wordElement = document.createElement('p');
+        wordElement.textContent = word;
+        newdiv.appendChild(wordElement);
+    });
+    document.body.appendChild(newdiv);
+    document.getElementById('words').style.display='none';
+}
+addDiv();
